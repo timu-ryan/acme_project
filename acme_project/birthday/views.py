313fd8d799cd78b887a0bd94016c106194cf3019
Tim Gafarov <timu.ryan@yandex.ru>
2025-04-14 23:17:35 +0300

    feat: rewrite code using CBV - class based views

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .forms import BirthdayForm
 from .models import Birthday
 
 from .utils import calculate_birthday_countdown
+
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
 
 
 def birthday(request, pk=None):

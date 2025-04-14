49db263a0688f3540a7557f2123b13ef35dfddf2
Tim Gafarov <timu.ryan@yandex.ru>
2025-04-15 02:33:52 +0300

    fix: fix auth part

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
-    'django_bootstrap5'
+    'django_bootstrap5',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
---
+++ acme_project/users/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+# Получаем модель пользователя:
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'bio')

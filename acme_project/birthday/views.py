476e2f64e663896498ac4f5b1eda2f10df73c661
Tim Gafarov <timu.ryan@yandex.ru>
2025-04-14 22:42:29 +0300

    feat: add pagination

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
 
 from .forms import BirthdayForm
 from .models import Birthday
@@ -28,8 +29,12 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 
